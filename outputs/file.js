7577e6259b486a9d53f847c567f512dba9f276d7
Weslley Silva <weslleycsil@gmail.com>
2018-10-11 18:46:50 -0300

    fix

--- outputs/file.js
+++ outputs/file.js
@@ -6,14 +6,14 @@ module.exports = function (config) {
       .on('error', function(err) {
         done(err);
       })
+      .on('end', function (stdout, stderr, err) {
+        job.progress(100, 100)
+        done()
+      })
       .screenshots({
         count: 1,
         folder: job.data.thumb.path,
         filename: job.data.thumb.filename,
-      })
-      .on('end', function (stdout, stderr, err) {
-        job.progress(100, 100)
-        done()
       })
 
     job.on('failed', function() {
@@ -22,7 +22,6 @@ module.exports = function (config) {
 
     function onProgress (progress) {
       job.progress(Math.round(progress.percent), 100);
-      console.log(Math.round(progress.percent));
     }
   }
 }
